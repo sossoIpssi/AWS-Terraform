47cc8a6df64d440699650b1e13e383af31c80d75
sossoIpssi <124569794+sossoIpssi@users.noreply.github.com>
2025-10-16 15:08:01 +0200

    Update main.tf

--- 05_hello_module/EC2/main.tf
+++ 05_hello_module/EC2/main.tf
@@ -1,1 +1,12 @@
-
+resource "aws_instance" "web" {
+  ami                         = var.ami
+  instance_type               = var.instance_type
+  key_name                    = var.key_name
+  subnet_id                   = var.subnet_id
+  vpc_security_group_ids      = [var.security_group_id]
+  associate_public_ip_address = true
+ 
+  tags = {
+    Name = "var.instance_name"
+  }
+}
