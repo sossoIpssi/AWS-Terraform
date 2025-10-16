212b1235fd6fc82c64fff3a831b8406de6896c41
sossoIpssi <124569794+sossoIpssi@users.noreply.github.com>
2025-10-16 15:07:31 +0200

    Update main.tf

--- 05_hello_module/main.tf
+++ 05_hello_module/main.tf
@@ -1,1 +1,23 @@
+provider "aws" {
+  region = "us-east-1"
+}
+
+module "vpc" {
+  source             = "./vpc"
+  vpc_cidr        = var.vpc_cidr
+  public_subnet_cidr = var.public_subnet_cidr
+  private_subnet_cidr = var.private_subnet_cidr
+  availability_zone  = var.availability_zone
+  vpc_name = "my-vpc"
+}
+
+module "ec2" {
+  source            = "./ec2"
+  ami               = var.ami
+  instance_type     = var.instance_type
+  subnet_id         = module.vpc.public_subnet_id
+  security_group_id = module.vpc.groupe_securite_id
+  key_name          = var.key_name
+  instance_name = "web"
+}
 
